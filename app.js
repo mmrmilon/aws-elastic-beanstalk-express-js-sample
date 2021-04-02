53657db50fe538b7bb453777e3bff6acf535f731
Mizanur Rahman <mr.milonku@gmail.com>
2021-04-02 17:19:41 +0600

    change message again

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('AWS DevOps, I am Milon!'));
+app.get('/', (req, res) => res.send('AWS DevOps, I have done all the settings'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
